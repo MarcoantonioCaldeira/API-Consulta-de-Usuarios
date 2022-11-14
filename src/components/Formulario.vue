83c4fb94e798497a03f7b6ca55232c5c0fe29f56
Marco Antonio Caldeira <caldeiran391@gmail.com>
2022-11-14 09:06:54 -0300

    resolvendo erros

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -21,28 +21,22 @@ export default {
     }
   },
   methods: {
-
     consulta_cpf(){
-      const access_token = localStorage.getItem('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJULkkuIEdlc3RvciIsInN1YiI6IjUwNWJhNDJlYTQ1NTUzNzYwNzkwMjk4NDc4ZDJmYmY0ZDA3OTFhMDIiLCJleHAiOjE2NjgyNjUyODV9.7ssWY3z34Mgu-ZITmx2MBm6cF-sONdePPdhA2dVbHA8');
-      const AuthStr =  'Bearer'.concat(access_token);
-      const url ='http://localhost:9000/auth';
       var self = this;
+      const AuthStr =  'Bearer'.concat();
+      const url ='http://localhost:9000/auth';
 
       axios.get(url,
-       { headers: { Authorization: AuthStr } })
+       { headers: { Authorization: AuthStr } + self.$session.get('token')})
       .then(function (response){
 
         console.log(response);
         self.dados_cpf = response.data;
         self.cpf_keys = Object.keys( self.dados_cpf );
-
       })
       .catch(function (error){
         console.log(error);
       })
-      .finally(function (){
-
-      });
     }
   }
 }
